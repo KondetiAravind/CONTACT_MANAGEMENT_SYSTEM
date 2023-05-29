5d878fc9eb6d3d4af4b4b5eda4e3ec811ecfb1c3
KondetiAravind <134742762+KondetiAravind@users.noreply.github.com>
2023-05-29 20:59:47 +0530

    Add files via upload

---
+++ CONTACTS MANAGEMENT SYSTEM IN C.c
@@ -0,0 +1,165 @@
+#include<stdio.h>
+#include<string.h>
+
+#define CONTACTS 10
+
+struct Contact
+{
+   char name[50],phone[15],email[30];
+};
+
+struct Contact contacts[CONTACTS];
+int count=0;
+
+void menu()
+{
+    printf("1.Add Contact\n");
+    printf("2.View Contacts\n");
+    printf("3.Delete Contact\n");
+    printf("4.Edit Contact\n");
+    printf("5.Find Contact\n");
+    printf("6.Exit\n");
+    printf("\n");
+}
+
+void add()
+{
+    if (count>=CONTACTS)
+    {
+        printf("Contact list is full\n");
+        return;
+    }
+    struct Contact c;
+    printf("Enter name: ");
+    scanf("%s",c.name);
+    printf("Enter phone number: ");
+    scanf("%s",c.phone);
+    printf("Enter email: ");
+    scanf("%s",c.email);
+    contacts[count++] = c;
+    printf("\n");
+}
+
+void find()
+{
+    char name[50];
+    printf("Enter name to find: ");
+    scanf("%s", name);
+    int i,found = 0;
+    for (i=0;i<count;i++)
+    {
+        if (strcmp(contacts[i].name,name)==0)
+        {
+            printf("Contact was found\n");
+            printf("Name=%s\tPhone No=%s\tEmail=%s\n", contacts[i].name,contacts[i].phone, contacts[i].email);
+            found = 1;
+            break;
+        }
+    }
+    if (!found)
+    {
+        printf("Contact not found\n");
+    }
+    printf("\n");
+}
+
+void del()
+{
+    char name[50];
+    printf("Enter name to delete: ");
+    scanf("%s", name);
+    int i,found=0;
+    for (i=0;i<count; i++)
+    {
+        if (strcmp(contacts[i].name, name) == 0)
+        {
+        	int j;
+            for ( j=i;j<count-1;j++)
+            {
+                contacts[j]=contacts[j + 1];
+            }
+            count--;
+            found=1;
+            printf("Contact deleted successfully\n");
+            break;
+        }
+    }
+    if(!found)
+    {
+        printf("Contact not found\n");
+    }
+    printf("\n");
+}
+
+void view()
+{
+	int i;
+	if(count!=0)
+	{
+    for (i=0;i<count;i++)
+    {
+        printf("name=%s\tphoneno=%s\tEmail=%s\n",contacts[i].name,contacts[i].phone,contacts[i].email);
+    }
+    }
+    else
+       printf("\n no contacts are there:\n");
+       printf("\n");
+}
+
+void edit()
+{
+    char name[50];
+    printf("Enter name to edit: ");
+    scanf("%s", name);
+    int i,found = 0;
+    for (i=0;i<count;i++)
+    {
+        if (strcmp(contacts[i].name,name) == 0)
+        {
+            printf("Enter new name: ");
+            scanf("%s", contacts[i].name);
+            printf("Enter new phone number: ");
+            scanf("%s", contacts[i].phone);
+            printf("Enter new email: ");
+            scanf("%s", contacts[i].email);
+            found = 1;
+            printf("Contact edited successfully\n");
+            break;
+        }
+    }
+    if (!found)
+        {
+            printf("Contact not found\n");
+        }
+        printf("\n");
+}
+
+int main()
+{
+    int choice;
+    do
+    {
+        menu();
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        switch (choice)
+            {
+                case 1:  add();
+                         break;
+                case 2:  view();
+                         break;
+                case 3:  del();
+                         break;
+                case 4:  edit();
+                         break;
+                case 5:  find();
+                         break;
+                case 6:  printf("Exit\n");
+                         break;
+                default:  printf("Invalid choice\n");
+                          printf("\n");
+                          break;
+            }
+    } while (choice!= 6);
+    return 0;
+}
